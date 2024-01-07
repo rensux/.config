4881f54bc9042bab51cea5a27e43abcd076a0078
rensux <renzo.russel@gmail.com>
2024-01-07 17:02:43 +0100

    telescope searches in current directory whether editing file or in oil

--- nvim/lua/rensux/config/telescope.lua
+++ nvim/lua/rensux/config/telescope.lua
@@ -1,7 +1,19 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+local get_dir = function()
+    local dir = vim.fn.expand("%:p:h")
+    if string.sub(dir, 1, 3) == "oil" then
+        return string.sub(dir, 7, -1) -- we are in oil (aka a directory) so remove oil:// prefix
+    else
+        return dir
+    end
+end
+
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ cwd = get_dir(), hidden = true })
+end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
